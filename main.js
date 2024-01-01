8c560434777d4b9d15b550153b1bc6e3189a188e
nikitafilipenko2 <you@example.com>
2024-01-01 21:42:16 +0300

    Reviews section

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
 
+    $('.reviews-slider').slick({
+        infinite: true,
+        speed: 500,
+        prevArrow: $("#reviews-previous"),
+        nextArrow: $("#reviews-next"),
+        fade: true,
+        swipe: false,
+        draggable: false,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
 
+    $('.reviews-slider').on('afterChange', function(event, slick, currentSlide) {
+        $('#reviews-number').text('0' + (currentSlide + 1));
+    });
     //First slider
     $("#wwu-slider-1").slick({
         infinite: true,
